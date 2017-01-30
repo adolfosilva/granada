0113b33ac6027ab26909e6b5a423af7b872a4d15
Adolfo Silva <a9126@alunos.ipca.pt>
2017-01-30 22:31:10 +0000

    Benchmarks... once more with feeling

--- bench/Parser.hs
+++ bench/Parser.hs
@@ -1,18 +1,18 @@
 module Parser (benchmarks) where
 
 import Criterion (Benchmark, bench, whnf)
-import qualified Granada
+import Text.Parsec (parse)
+import qualified Text.Parsec.Error
 
--- The function we're benchmarking.
-fib m | m < 0     = error "negative!"
-      | otherwise = go m
-  where
-    go 0 = 0
-    go 1 = 1
-    go n = go (n-1) + go (n-2)
+import Granada.Parser (action)
+import qualified Granada.Expr
+
+action01 :: String
+action01 = "action Attack { pre: { loadedWeapon: true }, post: { enemyDamage: 10 }"
+
+actionParse01 :: String -> Either Text.Parsec.Error.ParseError Granada.Expr.Action
+actionParse01 = parse action "actionParse01" 
 
 -- Our benchmark harness.
-benchmarks = [ bench "1"  $ whnf fib 1
-             , bench "5"  $ whnf fib 5
-             , bench "9"  $ whnf fib 9
-             , bench "11" $ whnf fib 11 ]
+benchmarks :: [Benchmark]
+benchmarks = [ bench "1"  $ whnf actionParse01 action01 ]
